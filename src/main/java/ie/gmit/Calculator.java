b557f6f87f0374f937ae845e07a6c5710ca10156
Krzysztof <chris1oficial@gmail.com>
2021-02-11 10:27:59 +0000

    First commit in my Calculator repository

---
+++ src/main/java/ie/gmit/Calculator.java
@@ -0,0 +1,14 @@
+package ie.gmit;
+
+import java.util.*;
+
+public class Calculator {
+
+
+
+        public int add(int firstNum, int secondNum) {
+            return firstNum + secondNum;
+        }
+
+
+}
